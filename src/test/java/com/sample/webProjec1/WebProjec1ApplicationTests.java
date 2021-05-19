6c3b737f4504d0e3398d2ff00c7861ebdfa0ea10
Sudhakar Reddy K <sudhakar.k@sritindia.com>
2021-05-19 21:09:53 +0530

    initail commit

---
+++ src/test/java/com/sample/webProjec1/WebProjec1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.webProjec1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjec1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
